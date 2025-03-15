d6e66638a37bee4b64ce8eb602e73b48df9133fa
Colin Balfour <75389927+ColinBalfour@users.noreply.github.com>
2025-03-15 16:36:43 -0400

    updated initial nerf code for turing deployment

--- Phase2/NeRFModel.py
+++ Phase2/NeRFModel.py
@@ -4,22 +4,65 @@ import numpy as np
 
 
 class NeRFmodel(nn.Module):
-    def __init__(self, embed_pos_L, embed_direction_L):
+    def __init__(self, embed_pos_L, embed_direction_L, num_channels=256):
         super(NeRFmodel, self).__init__()
         #############################
         # network initialization
         #############################
+        
+        self.embed_pos_L = embed_pos_L
+        self.embed_direction_L = embed_direction_L
+        
+        self.fc1 = nn.Sequential(
+            nn.Linear(embed_pos_L, 256),
+            nn.ReLU(),
+        )
+        
+        self.block1 = nn.Sequential(
+            nn.Linear(num_channels, num_channels), # 2
+            nn.ReLU(),
+            nn.Linear(num_channels, num_channels), # 3
+            nn.ReLU(),
+            nn.Linear(num_channels, num_channels), # 4
+            nn.ReLU(),
+            nn.Linear(num_channels, num_channels), # 5
+            nn.ReLU(),
+            nn.Linear(num_channels, num_channels), # 6
+            nn.ReLU(),
+            nn.Linear(num_channels, num_channels), # 7
+            nn.ReLU(),
+            nn.Linear(num_channels, num_channels), # 8
+            nn.ReLU()
+        )
+        
+        self.density_fc = nn.Linear(num_channels, 1)
+        
+        self.features_fc = nn.Sequential(
+            nn.Linear(num_channels + embed_direction_L, 128),
+            nn.ReLU(),
+            nn.Linear(128, 3),
+            nn.Sigmoid()
+        )
+        
+        
 
     def position_encoding(self, x, L):
         #############################
         # Implement position encoding here
         #############################
 
-        return y
+        return 
 
     def forward(self, pos, direction):
         #############################
         # network structure
         #############################
+        
+        out = self.fc1(pos)
+        out = self.block1(out)
+        density = self.density_fc(out)
+        
+        dir_input = torch.cat((out, direction), dim=-1)
+        rgb = self.features_fc(dir_input)
 
-        return output
+        return density, rgb
--- Phase2/Wrapper.py
+++ Phase2/Wrapper.py
@@ -7,6 +7,7 @@ import imageio
 import torch
 import matplotlib.pyplot as plt
 import os
+import json
 
 from NeRFModel import *
 
@@ -23,6 +24,42 @@ def loadDataset(data_path, mode):
         images: images
         pose: corresponding camera pose in world frame
     """
+    
+    images = []
+    poses = []
+    
+    json_file = os.path.join(data_path, f"transforms_{mode}.json")
+    
+    print(f"Loading data from {json_file} for {mode}")
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+        camera_angle_x = data['camera_angle_x']
+        frames = data['frames']
+    
+    for frame in frames:
+        file_path = os.path.join(data_path, frame['file_path'] + ".png")
+        img = imageio.imread(file_path)
+        if img.shape[-1] == 4:
+            img = img[..., :3]
+            
+        images.append(img)
+        poses.append(frame['transform_matrix'])
+        
+    focal = 0.5 * camera_angle_x * img.shape[1] / np.tan(0.5 * camera_angle_x)
+    
+    mtx = np.array([[focal, 0, img.shape[1] / 2],
+                    [0, focal, img.shape[0] / 2],
+                    [0, 0, 1]])
+
+    camera_info = {
+        'width': img.shape[1],
+        'height': img.shape[0],
+        'camera_matrix': mtx
+    }
+        
+        
+    return np.array(images), np.array(poses), camera_info
+    
 
 def PixelToRay(camera_info, pose, pixelPosition, args):
     """
@@ -34,6 +71,29 @@ def PixelToRay(camera_info, pose, pixelPosition, args):
     Outputs:
         ray origin and direction
     """
+    
+    pixel_x, pixel_y = pixelPosition
+    width, height = camera_info['width'], camera_info['height']
+    fx = camera_info['camera_matrix'][0][0]
+    fy = camera_info['camera_matrix'][1][1]
+    cx = camera_info['camera_matrix'][0][2]
+    cy = camera_info['camera_matrix'][1][2]
+    
+    # get pixel position in camera frame
+    pixel_x = (pixel_x - cx) / fx
+    pixel_y = (pixel_y - cy) / fy
+    pixel_z = 1.0
+    
+    pixel_position = np.array([pixel_x, pixel_y, pixel_z])
+    
+    # get ray direction in world frame
+    ray_direction = np.dot(pose[:3, :3], pixel_position)
+    ray_direction = ray_direction / np.linalg.norm(ray_direction)
+    
+    # get ray origin in world frame
+    ray_origin = pose[:3, 3]
+    
+    return ray_origin, ray_direction
 
 def generateBatch(images, poses, camera_info, args):
     """
@@ -45,8 +105,23 @@ def generateBatch(images, poses, camera_info, args):
     Outputs:
         A set of rays
     """
+    
+    rays = []
+    width = int(camera_info['width'])
+    height = int(camera_info['height'])
+    
+    for i in range(args.n_rays_batch):
+        image_index = random.randint(0, len(images) - 1)
+        pixelPosition = (random.randint(0, width - 1), random.randint(0, height - 1))
+        origin, direction = PixelToRay(camera_info, poses[image_index], pixelPosition, args)
+        rgb = images[image_index][pixelPosition[1], pixelPosition[0]]
+        # print([origin, direction, rgb])
+        rays.append(np.concatenate([origin, direction, rgb], axis=0))
+        
+    return np.array(rays, dtype=np.float32)
+        
 
-def render(model, rays_origin, rays_direction, args):
+def render(model, rays_origin, rays_direction, args, near=1.0, far=10.0):
     """
     Input:
         model: NeRF model
@@ -55,15 +130,82 @@ def render(model, rays_origin, rays_direction, args):
     Outputs:
         rgb values of input rays
     """
+    # print(rays_direction.shape, rays_origin.shape) # (N, 3) (N, 3)
+    
+    # sample points along ray
+    t_vals = torch.linspace(0, 1, args.n_sample)
+    t_vals = near + (far - near) * t_vals
+    t_vals = t_vals.unsqueeze(0).repeat(rays_origin.shape[0], 1).to(device)
+    
+    # get the delta for every sample
+    delta_t = t_vals[:, 1:] - t_vals[:, :-1]
+    delta_t = torch.cat([delta_t, torch.ones(delta_t.shape[0], 1).to(device)], dim=-1)
+    
+    # get 3d coords
+    rays_direction = rays_direction.unsqueeze(1).repeat(1, args.n_sample, 1)
+    rays_origin = rays_origin.unsqueeze(1).repeat(1, args.n_sample, 1)
+    # print(rays_direction.shape, rays_origin.shape, t_vals.shape) # (N, n_sample, 3) (N, n_sample, 3) (N, n_sample)
+    rays_points = rays_origin + rays_direction * t_vals.unsqueeze(-1)
+    
+    densities, rgbs = model(rays_points, rays_direction)
+    
+    # print(densities.shape, rgbs.shape, delta_t.shape) # (N, n_sample, 1) (N, n_sample, 3) (N, n_sample)
+    
+    alphas = 1.0 - torch.exp(-densities * delta_t.unsqueeze(-1))
+    weights = alphas * torch.cumprod(1.0 - alphas + 1e-10, dim=-1)
+    
+    # print(weights.shape) # (N, n_sample, 1)
+        
+    prediction = torch.sum(weights * rgbs, dim=1)
+    
+    # print(prediction.device)
+    
+    return prediction
 
 def loss(groundtruth, prediction):
-
+    return nn.MSELoss()(groundtruth, prediction)
 
 def train(images, poses, camera_info, args):
     
+    model = NeRFmodel(3, 3).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lrate)
+    writer = SummaryWriter(args.logs_path)
+    
+    try:
+    
+        for i in tqdm(range(args.max_iters)):
+            rays = generateBatch(images, poses, camera_info, args)
+            rays = torch.tensor(rays).to(device)
+            
+            rays_origin = rays[:, :3]
+            rays_direction = rays[:, 3:6]
+            rays_rgb = rays[:, 6:]
+            
+            prediction = render(model, rays_origin, rays_direction, args)
+            
+            loss_value = loss(rays_rgb, prediction)
+            
+            optimizer.zero_grad()
+            loss_value.backward()
+            optimizer.step()
+            
+            if i % 100 == 0:
+                writer.add_scalar('loss', loss_value.item(), i)
+                
+            if i % args.save_ckpt_iter == 0:
+                torch.save(model.state_dict(), os.path.join(args.checkpoint_path, f"model_{i}.pth"))
+        
+    except KeyboardInterrupt:
+        print("Training interrupted, saving checkpoint...")
+    finally:
+        writer.close()
+        torch.save(model.state_dict(), os.path.join(args.checkpoint_path, "final_model.pth"))
+    
+    return
 
 def test(images, poses, camera_info, args):
 
+    return
 
 def main(args):
     # load data
@@ -80,16 +222,16 @@ def main(args):
 
 def configParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path',default="./Phase2/Data/lego/",help="dataset path")
+    parser.add_argument('--data_path',default="./Phase2/nerf_synthetic/lego/",help="dataset path")
     parser.add_argument('--mode',default='train',help="train/test/val")
     parser.add_argument('--lrate',default=5e-4,help="training learning rate")
     parser.add_argument('--n_pos_freq',default=10,help="number of positional encoding frequencies for position")
     parser.add_argument('--n_dirc_freq',default=4,help="number of positional encoding frequencies for viewing direction")
     parser.add_argument('--n_rays_batch',default=32*32*4,help="number of rays per batch")
-    parser.add_argument('--n_sample',default=400,help="number of sample per ray")
+    parser.add_argument('--n_sample',default=50,help="number of sample per ray")
     parser.add_argument('--max_iters',default=10000,help="number of max iterations for training")
     parser.add_argument('--logs_path',default="./logs/",help="logs path")
-    parser.add_argument('--checkpoint_path',default="./Phase2/example_checkpoint/",help="checkpoints path")
+    parser.add_argument('--checkpoint_path',default="./Phase2/checkpoints/",help="checkpoints path")
     parser.add_argument('--load_checkpoint',default=True,help="whether to load checkpoint or not")
     parser.add_argument('--save_ckpt_iter',default=1000,help="num of iteration to save checkpoint")
     parser.add_argument('--images_path', default="./image/",help="folder to store images")
